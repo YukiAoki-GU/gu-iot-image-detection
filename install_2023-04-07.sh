b0f1982a4686d569a153c68931ab1aabde421d1c
YukiAoki-GU <63085006+YukiAoki-GU@users.noreply.github.com>
2023-04-07 18:16:12 +0900

    Add files via upload

---
+++ install_2023-04-07.sh
@@ -0,0 +1,10 @@
+cd ~/
+sudo apt update -y
+python3 -m pip install tflite-runtime
+cd ~/.node-red
+npm install python-shell@3.0.1
+cd ~/
+git clone
+cd .node-red
+npm install ~/gu-iot-image-detection
+sudo systemctl restart nodered
